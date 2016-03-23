99cadd4e37ba0cd7ddd9ed281d797f53e37e0138
Jon <powers.j.email@gmail.com>
2016-03-23 08:12:41 -0400

    i have varried the animation on the string

---
+++ Assets/scripts/AnimationVary.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationVary : MonoBehaviour {
+    int var;
+    Animator localAnim;
+    float animTime;
+    bool anim_Play;
+    // Use this for initialization
+    void Start () {
+        //anim_Play = false;
+        localAnim = GetComponent<Animator>();
+        var = UnityEngine.Random.Range(0, 1);
+        localAnim.SetFloat("Direction", 1);
+        if (var == 1 || var == 0) //reverse play an animation
+        {
+            localAnim.Play("RopeAnimator");
+            Debug.Log("Fuck you dolphin");
+        } else
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (anim_Play && animTime < 1)           //If animation is toggled on
+        //    animTime += 0.3f * Time.deltaTime;   //Increase animTime, Don't let animTime go beyond 1
+        //else if (!anim_Play && animTime > 0)     //If animation is toggled off/reversed
+        //    animTime -= 0.3f * Time.deltaTime;   //Decrease animTime, Don't let animTime go below 0
+
+        //GetComponent<Animator>().Play("RopeAnimator", 0, animTime);
+    }
+}
---
+++ Assets/scripts/ChangeDirection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeDirection : StateMachineBehaviour {
+    int var;
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        var = UnityEngine.Random.Range(0, 2);
+        if (var == 1)
+        {
+            animator.SetFloat("Direction", -1.0f);
+            //animator.Play("RopeAnimation", 1, UnityEngine.Random.Range(0.0f, 1.0f));
+        }
+        animator.speed = UnityEngine.Random.Range(1, 3); ;
+    }
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
